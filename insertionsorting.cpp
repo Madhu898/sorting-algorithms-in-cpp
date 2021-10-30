92cbec9eb6f7af1d49a287adc2019837b31be0cd
Madhusudhan <79078323+Madhu898@users.noreply.github.com>
2021-10-30 16:40:24 +0530

    Update insertionsorting.cpp

--- insertionsorting.cpp
+++ insertionsorting.cpp
@@ -1,35 +1,33 @@
-\\code was taken from code with harry in his tutorial of datastructures and algorithms
-\\simplified algorithms
-#include<iostream>
+#include <iostream>
+#include <stdlib.h>
 using namespace std;
 int main()
 {
     int n;
-    cout<<"enter the size of an array "<<endl;
-    
-    cin>>n;
+    system("cls");
+    cout << "enter the size of the array " << endl;
+    cin >> n;
     int a[n];
-    cout<<"enter the elements in to an array "<<endl;
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        cin>>a[i];
+        cin >> a[i];
     }
-    int j,key;
-    for(int i=1;i<n;i++)
+
+    for (int i = 1; i < n; i++)
     {
-        j=i-1;
-        key=a[i];
-        while(j>=0&&a[j]>key)
+        int current = a[i];
+        int j = i - 1;
+        while (a[j] > current && j >= 0)
         {
-            a[j+1]=a[j];
+            a[j + 1] = a[j];
             j--;
         }
-        a[j+1]=key;
+        a[j + 1] = current;
     }
-    cout<<"array after sorting  "<<endl;
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        cout<<a[i]<<" ";
+        cout << a[i] << " ";
     }
+
     return 0;
 }
