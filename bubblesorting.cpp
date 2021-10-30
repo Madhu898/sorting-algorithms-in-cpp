03f73bdb91b4a09a7350ef5b50986da911e02892
Madhusudhan <79078323+Madhu898@users.noreply.github.com>
2021-10-30 09:22:56 +0530

    Update bubblesorting.cpp

--- bubblesorting.cpp
+++ bubblesorting.cpp
@@ -12,9 +12,9 @@ int main()
         cin>>a[i];
     }
     int count=1;
-    while(count<n)
+    while(count<n-1)
     {
-       for(int i=0;i<n;i++)
+       for(int i=0;i<n-count;i++)
        {
            if(a[i]>a[i+1])
            {
