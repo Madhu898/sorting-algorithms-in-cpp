8462c4f7af6411724031b69efb0a541b8b8e2458
Madhusudhan <79078323+Madhu898@users.noreply.github.com>
2021-10-30 14:44:42 +0530

    Update bubblesorting.cpp

--- bubblesorting.cpp
+++ bubblesorting.cpp
@@ -1,31 +1,43 @@
+//comapares adjcent items in array and swaps accordingly 
+// biggest of all numbers sent to last in first iteration 
+
 #include<iostream>
+#include<stdlib.h>
 using namespace std;
 int main()
 {
-    int n;
+    int n,sorted=0;
+	system("cls");
     cout<<"enter the size of an array "<<endl;
     cin>>n;
     int a[n];
-    cout<<"enter the elements in to an array "<<endl;
+    cout<<"enter elements in to array "<<endl;
     for(int i=0;i<n;i++)
     {
         cin>>a[i];
     }
-    int count=1;
+    int count=0;
     while(count<n-1)
     {
-       for(int i=0;i<n-count;i++)
+		cout<<"in pass "<<count+1<<endl;
+       sorted=1;
+	   for(int i=0;i<n-count;i++)
        {
            if(a[i]>a[i+1])
            {
                int temp=a[i];
                a[i]=a[i+1];
                a[i+1]=temp;
+			   sorted=0;
            }
        } 
+	   if(sorted)
+	   {
+		  break;
+	   }
        count++;
     }
-    cout<<"enter after sorting  "<<endl;
+    cout<<"after sorting  "<<endl;
     for(int i=0;i<n;i++)
     {
         cout<<a[i]<<" ";
